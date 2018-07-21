d6bf66d7dfab1fe83ad32da0f056f071f4fc7a92
Jon Yardley <jon.yardley@red-badger.com>
2018-07-21 19:18:12 +0100

    Disable mobile zoom

--- layout.js
+++ layout.js
@@ -6,7 +6,10 @@ export default ({ children, title = "WebMidi Talk" }) => (
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
     </Head>
     {children}
   </div>
